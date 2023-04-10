c42bf1e8aa9de4a24cc94ef9e41f681c30d65e3a
rbahena <rbahena@cjf.gob.mx>
2023-04-09 19:31:58 -0600

    Update content and script function

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function readingTime() {
     const text = document.getElementById("article").innerText;
     const wpm = 225;
     const words = text.trim().split(/\s+/).length;
-    const time = Math.abs(words / wpm).toFixed(2);
+    const time = Math.abs(words / wpm).toFixed(0);
     document.getElementById("time").innerText = time;
   }
   readingTime();
